f54bec3077061024bf707e2f41f33f9d94f3d69f
Luis Hernández <luishernandez.mail@gmail.com>
2015-04-10 19:03:35 +0200

    Updated OL doc

--- widget/OL.php
+++ widget/OL.php
@@ -6,41 +6,7 @@ use yii\helpers\Json;
 
 /**
  * This class makes it easier to specify OpenLayers objects as options for the OpenLayers widget, resulting in more concise PHP array structures than when using JsExpression.
- * The OL class takes a classname and an optional property array as constructor arguments. Then, when an OL object is passed to Json::encode(), the following expression will be generated:
- * <code>new ol.$class($properties)</code>.
- * Therefore, the need for JsExpression is eliminated, and the 'ol.' namespace, the 'new' keyword and constructor call are automatically added.
- * For example, the following mapOptions array:
- * <pre>//...
- * 'layers' => [
- *   new OL('layer.Tile', [
- *		'source' => new OL('source.MapQuest', [
- *			'layer' => 'sat',
- *		]),
- *	 ]),
- * ],
- * 'view' => [
- * 	 new OL('View', [
- * 		'center' => [0, 0],
- * 		'zoom' => 4,
- * 	],
- * ]
- * /...</pre>
- * Will be encoded as follows:
- * <pre>
- * {
- * 	"layers": [
- * 		new ol.layer.Tile({
- * 			"source": new ol.source.MapQuest({
- * 				"layer": "sat"
- * 			})
- * 		})
- * 	],
- * 	"view": new ol.View({
- * 		"center": [0, 0],
- * 		"zoom": 4
- * 	})
- * }
- * </pre>
+ * See {@link OL::__construct} for usage examples.
  * @link https://github.com/Sibilino/yii2-openlayers
  * @copyright Copyright (c) 2015 Luis Hernández Hernández
  * @license http://opensource.org/licenses/MIT MIT
@@ -57,6 +23,45 @@ class OL extends JsExpression
 	 */
 	protected $properties;
 	
+	/**
+	 * The OL class takes a classname and an optional property array as constructor arguments. Then, when an OL object is passed to Json::encode(), the following expression will be generated:
+	 * <code>new ol.$class($properties)</code>.
+	 * Therefore, the need for JsExpression is eliminated, and the 'ol.' namespace, the 'new' keyword and constructor call are automatically added.
+	 * For example, the following mapOptions array:
+	 * <pre>//...
+	 * 'layers' => [
+	 *   new OL('layer.Tile', [
+	 *		'source' => new OL('source.MapQuest', [
+	 *			'layer' => 'sat',
+	 *		]),
+	 *	 ]),
+	 * ],
+	 * 'view' => [
+	 * 	 new OL('View', [
+	 * 		'center' => [0, 0],
+	 * 		'zoom' => 4,
+	 * 	],
+	 * ]
+	 * /...</pre>
+	 * Will be encoded as follows:
+	 * <pre>
+	 * {
+	 * 	"layers": [
+	 * 		new ol.layer.Tile({
+	 * 			"source": new ol.source.MapQuest({
+	 * 				"layer": "sat"
+	 * 			})
+	 * 		})
+	 * 	],
+	 * 	"view": new ol.View({
+	 * 		"center": [0, 0],
+	 * 		"zoom": 4
+	 * 	})
+	 * }
+	 * </pre>
+	 * @param string $class
+	 * @param array $properties
+	 */
 	public function __construct($class, array $properties = null)
 	{
 		$this->class = $class;
