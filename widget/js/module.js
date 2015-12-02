b8b35d96762a4fd9c4efbe10fbf7b8ed85b2fcf3
Luis Hernández <luishernandez.mail@gmail.com>
2015-12-02 12:19:58 +0100

    Module root is no longer overwritten

--- widget/js/module.js
+++ widget/js/module.js
@@ -1,34 +1,33 @@
-var sibilino = {
-    openlayers: (function ($) {
-        var pub = {
-            // whether this module is currently active. If false, init() will not be called for this module
-            // it will also not be called for all its child modules. If this property is undefined, it means true.
-            isActive: true,
-            init: function () {
-
-            },
-            maps: {},
-            // Storage for additional options that may be set by other javascript scripts
-            // Usage: sibilino.openlayers.mapOptions["identifier"] = { layers: [new ol.layer.Vector(...)] }
-            // Then, configure the OpenLayers widget with $jsOptionIdentifier = "identifier".
-            mapOptions: {},
-            createMap: function (options, id, jsOptionIdentifier) {
-                if (typeof id === "undefined") {
-                    id = options['target'];
-                }
-                if (typeof jsOptionIdentifier !== "undefined") {
-                    $.extend(true, options, this.mapOptions[jsOptionIdentifier]); // Deep merge
-                }
-                this.maps[id] = new ol.Map(options);
-            },
-            getMapById: function (id) {
-                return this.maps[id];
+if (typeof sibilino === "undefined") {
+    sibilino = {}; // Initialize sibilino as empty object if not existing
+}
+sibilino.openlayers = (function ($) {
+    var pub = {
+        isActive: true,
+        init: function () {
+            // Nothing to init
+        },
+        // Storage for additional options that may be set by other javascript scripts
+        // Usage: sibilino.openlayers.mapOptions["identifier"] = { layers: [new ol.layer.Vector(...)] }
+        // Then, configure the OpenLayers widget with $jsOptionIdentifier = "identifier".
+        mapOptions: {},
+        createMap: function (options, id, jsOptionIdentifier) {
+            if (typeof id === "undefined") {
+                id = options['target'];
             }
-        };
-        // ... private functions and properties go here ...
-        return pub;
-    })(jQuery)
-};
+            if (typeof jsOptionIdentifier !== "undefined") {
+                $.extend(true, options, mapOptions[jsOptionIdentifier]); // Deep merge
+            }
+            maps[id] = new ol.Map(options);
+        },
+        maps: {},
+        getMapById: function (id) {
+            return maps[id];
+        }
+    };
+    // ... private functions and properties go here ...
+    return pub;
+})(jQuery);
 
 jQuery(document).ready(function () {
     yii.initModule(sibilino);
